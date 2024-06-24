c6427bc21e855454fcb0c77dce5919c9aa9187fc
Korbut-Sergey <160742586+Korbut-Sergey@users.noreply.github.com>
2024-06-24 20:32:59 +0000

    add second homework

---
+++ lesson_2/lesson_2_task_4.py
@@ -0,0 +1,12 @@
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("DizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print (i)
+n = 20
+fizz_buzz(n)
