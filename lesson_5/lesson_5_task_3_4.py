a5288a8a431897068659a9eba7b56036fd8bcbc4
Korbut-Sergey <160742586+Korbut-Sergey@users.noreply.github.com>
2024-08-23 17:27:47 +0000

    add second homework

--- lesson_5/lesson_5_task_3_4.py
+++ lesson_5/lesson_5_task_3_4.py
@@ -13,15 +13,10 @@ try:
 
     ch_wait = WebDriverWait(chrome, 10)
     ff_wait = WebDriverWait(firefox, 10)
-    ch_close_button = ch_wait.until(EC.element_to_be_clickable(
-        By.CSS_SELECTOR, ".modal-footer"))
-    ff_close_button = ff_wait.until(EC.element_to_be_clickable(
-        By.CSS_SELECTOR, ".modal-footer"))
-    time.sleep(5)
-
-    ch_close_button.click()
-    ff_close_button.click()
-    time.sleep(3)
+    ch_wait.until(EC.element_to_be_clickable(
+        chrome.find_element(By.CSS_SELECTOR, '.modal-footer'))).click()
+    ff_wait.until(EC.element_to_be_clickable(
+        firefox.find_element(By.CSS_SELECTOR, '.modal-footer'))).click()
 
 except Exception as ex:
     print(ex)
