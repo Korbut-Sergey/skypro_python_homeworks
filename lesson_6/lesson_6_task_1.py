aff749592ef34c5c29006d1ebd2de4ef15d13046
Korbut-Sergey <160742586+Korbut-Sergey@users.noreply.github.com>
2024-08-25 22:12:51 +0000

    add second homework

--- lesson_6/lesson_6_task_1.py
+++ lesson_6/lesson_6_task_1.py
@@ -3,12 +3,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
 chrome = webdriver.Chrome()
 wait = WebDriverWait(chrome, 20, 0.1)
 
 try:
     chrome.get("http://uitestingplayground.com/ajax")
-    chrome.find_element(By.CSS_SELECTOR, "ajaxButton"). click()
+    
+    chrome.find_element(By.CSS_SELECTOR, "#ajaxButton"). click()
+    
     print(wait.until (EC.visibility_of_element_located(
         (By.CSS_SELECTOR, ".bg-success"))).text)
 
--- lesson_6/lesson_6_task_2.py
+++ lesson_6/lesson_6_task_2.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 
+
 chrome = webdriver.Chrome()
 
 try:
     chrome.get("http://uitestingplayground.com/textinput")
+    
     chrome.find_element("id", "newButtonName").send_keys("SkyPro")
+    
     chrome.find_element("id", "updatingButton").click()
+    
     print(chrome.find_element("id", "updatingButton").text)
 
 except Exception as ex:
